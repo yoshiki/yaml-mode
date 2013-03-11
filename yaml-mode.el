5eae0c19568b6947887e4f0bfef74cf58e6ec501
Yoshiki Kurihara <clouder@gmail.com>
2013-03-11 12:47:12 +0900

    Changed version to 0.0.9

--- yaml-mode.el
+++ yaml-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Yoshiki Kurihara <kurihara@cpan.org>
 ;;         Marshall T. Vandegrift <llasram@gmail.com>
 ;; Keywords: data yaml
-;; Version: 0.0.7
+;; Version: 0.0.9
 
 ;; This file is not part of Emacs
 
@@ -115,7 +115,7 @@ that key is pressed to begin a block literal."
 
 ;; Constants
 
-(defconst yaml-mode-version "0.0.7" "Version of `yaml-mode'.")
+(defconst yaml-mode-version "0.0.9" "Version of `yaml-mode'.")
 
 (defconst yaml-blank-line-re "^ *$"
   "Regexp matching a line containing only (valid) whitespace.")
